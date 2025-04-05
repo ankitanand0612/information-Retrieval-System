4b393f07af45fffb2a449c3d1b5242adbe64a20e
ankitanand0612 <40160224+ankitanand0612@users.noreply.github.com>
2025-04-05 20:35:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval System',
+    version= '0.0.0',
+    author= 'Ankit Anand',
+    author_email= 'ankitanand0612@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
